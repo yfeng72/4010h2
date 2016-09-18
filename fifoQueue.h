481f8c40aecfd7b526e46143ee554d621609b3f1
yfeng72 <fengy@gatech.edu>
2016-09-17 20:27:19 -0400

    reformatted code style

--- fifoQueue.h
+++ fifoQueue.h
@@ -1,21 +1,22 @@
 #ifndef __FifoQueue_H
 #define __FifoQueue_H
+
 #include "Node.c"
 
 typedef struct FifoQueue FifoQueue;
 
-struct FifoQueue{
-    Node * head;
+struct FifoQueue {
+    Node *head;
 };
 
-void insertNodeFifo(FifoQueue * queue, Node * inNode);
+void insertNodeFifo( FifoQueue *queue, Node *inNode );
 
-void removeFirstElementFifo(FifoQueue * queue);
+void removeFirstElementFifo( FifoQueue *queue );
 
-void printQueueFifo(FifoQueue * queue);
+void printQueueFifo( FifoQueue *queue );
 
-FifoQueue * createNewQueueFifo();
+FifoQueue *createNewQueueFifo();
 
-void freeFifoMem(FifoQueue * queue);
+void freeFifoMem( FifoQueue *queue );
 
 #endif
--- priorityQueue.c
+++ priorityQueue.c
@@ -1,22 +1,18 @@
 #include <stdlib.h>
-#include "priorityQueue.h" 
-#include <stdio.h>
+#include "priorityQueue.h"
 
 //Place smallest node vals at front
-void insertNode(PriorityQueue *queue, Node * inNode)
-{
-    printf("Inserting node\n");
-    Node * thisNode = queue->head;
-    if(queue->head == NULL)
-    {
+void insertNode( PriorityQueue *queue, Node *inNode ) {
+    printf( "Inserting node\n" );
+    Node *thisNode = queue->head;
+    if ( queue->head == NULL ) {
         queue->head = inNode;
         return;
     }
 
 
     //If we need to insert to very front of list
-    if(inNode->timestamp <= thisNode ->timestamp)
-    {
+    if ( inNode->timestamp <= thisNode->timestamp ) {
         queue->head = inNode;
         inNode->nextNode = thisNode;
         thisNode->prevNode = inNode;
@@ -24,16 +20,14 @@ void insertNode(PriorityQueue *queue, Node * inNode)
     }
 
 
-    while( (thisNode->timestamp) < (inNode -> timestamp))
-    {
-             
-        if(thisNode->nextNode == NULL)
-        {
+    while ((thisNode->timestamp) < (inNode->timestamp)) {
+
+        if ( thisNode->nextNode == NULL ) {
             thisNode->nextNode = inNode;
-            inNode->prevNode = thisNode;    
+            inNode->prevNode = thisNode;
             return;
         }
-        thisNode = thisNode -> nextNode;
+        thisNode = thisNode->nextNode;
     }
     thisNode->prevNode->nextNode = inNode;
     //thisNode->prevNode = inNode;
@@ -45,81 +39,69 @@ void insertNode(PriorityQueue *queue, Node * inNode)
 
 }
 
-void removeFirstElement(PriorityQueue * queue)
-{
-    Node * firstNode = queue->head;
-    if(firstNode == NULL)
+void removeFirstElement( PriorityQueue *queue ) {
+    Node *firstNode = queue->head;
+    if ( firstNode == NULL )
         return;
 
-    if(firstNode->nextNode == NULL)
-    {
-        free(firstNode);
+    if ( firstNode->nextNode == NULL ) {
+        free( firstNode );
         queue->head = NULL;
     }
-    else
-    {
-        Node * tmp = firstNode->nextNode;
-        free(firstNode);
+    else {
+        Node *tmp = firstNode->nextNode;
+        free( firstNode );
         tmp->prevNode = NULL;
         queue->head = tmp;
     }
 }
 
-void moveTopFromPriorityToFifo(PriorityQueue * priority, FifoQueue * fifo)
-{
+void moveTopFromPriorityToFifo( PriorityQueue *priority, FifoQueue *fifo ) {
     //Deletes top element of priority queue and pushes to back of fifo
-    Node * nodeToMove = priority->head;
-    Node * tmp = nodeToMove->nextNode;
+    Node *nodeToMove = priority->head;
+    Node *tmp = nodeToMove->nextNode;
     //nodeToMove->nextNode = NULL;
-    insertNodeFifo(fifo, priority->head);
+    insertNodeFifo( fifo, priority->head );
     //Seems like we left it dangling, but really just swapped to fifo and memory management will be handled there
     priority->head = tmp;
 }
 
 
-
-
-void printQueue(PriorityQueue * queue)
-{
-    Node * thisNode = queue->head;
-    while(thisNode != NULL)
-    {
-        printf("%f\n",thisNode->timestamp);
+void printQueue( PriorityQueue *queue ) {
+    Node *thisNode = queue->head;
+    while ( thisNode != NULL ) {
+        printf( "%f\n", thisNode->timestamp );
         thisNode = thisNode->nextNode;
     }
-    
+
 }
-void printQueueReverse(PriorityQueue * queue)
-{
-    Node * thisNode = queue->head;
-    while(thisNode->nextNode != NULL)
-    {
+
+void printQueueReverse( PriorityQueue *queue ) {
+    Node *thisNode = queue->head;
+    while ( thisNode->nextNode != NULL ) {
         thisNode = thisNode->nextNode;
     }
     //Since no tail pointer, iterate to back, then go backwards from there
-;    while(thisNode != NULL)
-    {
-        printf("Backwards: %f\n",thisNode->timestamp);
+    ;
+    while ( thisNode != NULL ) {
+        printf( "Backwards: %f\n", thisNode->timestamp );
         thisNode = thisNode->prevNode;
     }
 
 }
 
-PriorityQueue * createNewLinkedList()
-{
-    PriorityQueue * linkedList = malloc(sizeof(PriorityQueue)); 
-    linkedList -> head = NULL;
+PriorityQueue *createNewLinkedList() {
+    PriorityQueue *linkedList = malloc( sizeof( PriorityQueue ));
+    linkedList->head = NULL;
     return linkedList;
 }
 
-void freeMem(PriorityQueue * queue)
-{
-    Node * thisNode = queue->head;
-    while(thisNode != NULL)
-    {
-        Node * tmp = thisNode->nextNode;
-        free(thisNode);
+void freeMem( PriorityQueue *queue ) {
+    Node *thisNode = queue->head;
+    while ( thisNode != NULL ) {
+        Node *tmp = thisNode->nextNode;
+        free( thisNode );
         thisNode = tmp;
     }
-    free(queue);
+    free( queue );
 }
